7a55822126b0113938604f99391fedb35e9c0b52
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-16 16:41:27 +0300

    c

--- app/src/routes/+page.server.js
+++ app/src/routes/+page.server.js
@@ -3,8 +3,9 @@ import { ALL_CATALOG } from '$lib/GQL/query/test.js';
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
 	const query = ALL_CATALOG;
+	const key = import.meta.env.VITE_TOKEN;
 	const variables = {
-		key: '1'
+		key
 	};
 	const response1 = await request('http://127.0.0.1:8001/graphql/', query, variables);
 	if (response1) return { response1 };
