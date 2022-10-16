0dd6feb031c81fd08a77c5e6e50c10b2c86fdba5
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-16 15:22:13 +0300

    c

--- app/src/routes/+page.server.js
+++ app/src/routes/+page.server.js
@@ -3,8 +3,8 @@ import { request, gql } from 'graphql-request';
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
 	const query = gql`
-		query all_catalog {
-			catalog {
+		query all_catalog($key: String!) {
+			catalog(key: $key) {
 				id
 				value
 				is_active
@@ -19,7 +19,10 @@ export async function load() {
 			}
 		}
 	`;
-	const response1 = await request('http://127.0.0.1:8001/graphql/', query);
+	const variables = {
+		key: '1'
+	};
+	const response1 = await request('http://127.0.0.1:8001/graphql/', query, variables);
 	// console.log(response);
 	if (response1) return { response1 };
 
