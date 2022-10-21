ebb924af9a81ddcffaaf2a74c44013dc879cd503
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-21 17:57:26 +0300

    c

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -4,9 +4,8 @@
   import Header from "./header/index.svelte"
   import Footer from "./footer/index.svelte"
   export let data
-  // console.log(data);
 </script>
 
-<Header />
+<Header {...data.contentHeader}/>
   <slot />
 <Footer {...data.contentFooter}/>
--- app/src/routes/footer/index.svelte
+++ app/src/routes/footer/index.svelte
@@ -2,7 +2,6 @@
     /** @type {import('./$types').PageData} */
     export let content;
 </script>
-<!-- <pre>{data.contentFooter.content.yearStart}</pre> -->
 <footer class="bg-gray-50">
 	<div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
 		<nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
