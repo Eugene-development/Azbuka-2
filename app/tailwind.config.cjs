5a75d0c799be94ff2685e3379d7a60482cae911b
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-22 20:28:09 +0300

    с

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./node_modules/symax/**/*.{html,js,svelte,ts}', './src/**/*.{html,js,svelte,ts}'],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
-	plugins: []
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/aspect-ratio')
+	]
 };
