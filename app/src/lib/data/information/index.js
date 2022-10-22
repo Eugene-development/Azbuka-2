6184d91541a405ad121475e0dc9291e9b05650ff
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-22 14:12:22 +0300

    c

---
+++ app/src/lib/data/information/index.js
@@ -0,0 +1,15 @@
+const title = 'Доставка';
+const text =
+	'Привозим мебель своим транспортом до адреса заказчика. Есть услуга подъёма на этаж грузчиками. Также вы можете забрать заказ самовывозом.';
+const img =
+	'https://images.unsplash.com/photo-1507207611509-ec012433ff52?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80';
+
+const content = {
+	title,
+	text,
+	img
+};
+
+export const contentDelivery = {
+	content
+};
--- app/src/routes/information/dostavka/+page.js
+++ app/src/routes/information/dostavka/+page.js
@@ -1,4 +1,5 @@
+import { contentDelivery } from '$lib/data/information';
 /** @type {import('./$types').PageLoad} */
 export async function load() {
-	return {};
+	return { contentDelivery };
 }
