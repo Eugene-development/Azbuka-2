59788f3d242616a3089df757acd82d945b779bf8
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-22 14:48:50 +0300

    c

--- app/src/routes/information/oplata/+page.svelte
+++ app/src/routes/information/oplata/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+    import Information from "$lib/components/information/index.svelte"
     /** @type {import('./$types').PageData} */
     export let data;
 </script>
+
+<Information {...data.contentPayment}/>
