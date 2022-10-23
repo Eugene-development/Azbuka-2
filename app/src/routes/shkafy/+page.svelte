fe3ba2e4d35cd427b474f3a0dc225f1b96220070
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-23 13:22:56 +0300

    c

--- app/src/routes/shkafy/+page.svelte
+++ app/src/routes/shkafy/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+    import Shkafi from "$lib/components/rubric/index.svelte"
     /** @type {import('./$types').PageData} */
     export let data;
 </script>
+
+<Shkafi {...data.contentShkafi}/>
