e1a31f5f57469b923319af77ff605e080091c13d
indexpro24@gmail.com <indexpro24@gmail.com>
2022-10-24 13:02:42 +0300

    c

--- app/src/lib/data/contacts/index.js
+++ app/src/lib/data/contacts/index.js
@@ -1,3 +1,18 @@
+// BC
+const currentPage = 'Контакты';
+const title = 'Контакты';
+const description = 'Связаться с нами вы можете по почте, телефону или через чат.';
+const pathImg =
+	'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/1.jpg';
+
+const BC = {
+	currentPage,
+	title,
+	description,
+	pathImg
+};
+
+// Main
 const address = 'Нижний Новгород, Гордеевская 7';
 const phones = [
 	{
@@ -11,46 +26,24 @@ const emails = [
 	}
 ];
 
-const apiMail = import.meta.env.VITE_API_MAIL;
-const apiToken = import.meta.env.VITE_TOKEN;
-
-const header = 'Отправить запрос или коммерческое предложение:';
-
-const contentForm_v2 = {
-	apiMail,
-	apiToken,
-	header
-};
-
 const content = {
-	// contentForm_v2,
 	address,
 	phones,
 	emails
 };
 
+// const apiMail = import.meta.env.VITE_API_MAIL;
+// const apiToken = import.meta.env.VITE_TOKEN;
+
+// const header = 'Отправить запрос или коммерческое предложение:';
+
+// const contentForm_v2 = {
+// 	apiMail,
+// 	apiToken,
+// 	header
+// };
+
 export const contentContacts = {
+	BC,
 	content
 };
-// const currentPage = 'Контакты';
-
-// const title = 'Контакты';
-// const description = 'Связаться с нами вы можете по почте, телефону или через чат.';
-// const pathImg =
-// 	'https://storage.yandexcloud.net/brand-logo/mos-mebel/MarketingSectionV5/cabinets/1.jpg';
-
-// const content_Header_v3 = {
-// 	currentPage,
-// 	title,
-// 	description,
-// 	pathImg
-// };
-
-// export const GET = () => {
-// 	return {
-// 		body: {
-// 			content_Header_v3,
-// 			content_Contact_v1
-// 		}
-// 	};
-// };
--- app/src/routes/contacts/+page.svelte
+++ app/src/routes/contacts/+page.svelte
@@ -1,7 +1,9 @@
 <script>
+    import BreadCrumbs from "$lib/components/bread-crumbs/index.svelte"
     import Contacts from "$lib/components/contacts/index.svelte"
     /** @type {import('./$types').PageData} */
     export let data;
 </script>
 
+<BreadCrumbs {...data.contentContacts}/>
 <Contacts {...data.contentContacts}/>
